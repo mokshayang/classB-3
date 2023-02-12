4a81ca85cd67de823ccad980bf7550afb5cdce09
unknown <dullstart.yang.com>
2023-02-12 09:42:19 +0800

    兩個div 互相切換

--- front/order.php
+++ front/order.php
@@ -1,1 +1,33 @@
-線上訂票
+<h3 class="ct">線上訂票</h3>
+<div class="ord">
+    <table>
+        <tr>
+            <td>電影 :</td>
+            <td>
+               <select id="movs"></select> 
+            </td>
+        </tr>
+        <tr>
+            <td>日期 :</td>
+            <td>
+                <select id="days"></select>
+            </td>
+        </tr>
+        <tr>
+            <td>場次 :</td>
+            <td>
+                <select id="sess"></select>
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <button onclick="$('#ord,$booking').toggle();booking();">確認</button>
+        <button onclick="">重置</button>
+    </div>
+</div>
+<div id="booking">
+    <div class="ct">
+        <button onclick="$('#ord,$booking').toggle();">確定</button>
+        <button>上一步</button>
+    </div>
+</div>
