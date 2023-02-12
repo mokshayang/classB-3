450995b226889bf316ce5bb499f4dc86714c55d7
unknown <dullstart.yang.com>
2023-02-12 15:43:23 +0800

    加油啊

--- front/order.php
+++ front/order.php
@@ -23,14 +23,14 @@
     </table>
     <br>
     <div class="ct">
-        <button onclick="$('#ord,#booking').toggle(); ">確認</button>
+        <button onclick="$('#ord,#booking').toggle(); getBooking();">確認</button>
         <button onclick="">重置</button>
     </div>
 </div>
 
 <div id="booking" style="display:none">
     <div>你選擇的電影是 : <span id="mov"></span></div>
-    <div>你選擇的日期是 :<span id="day"></span>場次為 : <span class="sess"></span></div>
+    <div>你選擇的日期是 :<span id="day"></span> 場次為 :<span id="sess"></span></div>
     <div>你已選擇 <span id="tickets"> </span>張，最多可以購買四張。</div>
     <div class="ct">
         <button onclick="$('#ord,#booking').toggle();">上一步</button>
@@ -40,31 +40,44 @@
 <script>
     getMov();
 
-//聯動是呼叫 : 
-$('#movs').on('change',()=>{
-    getDay($('#movs').val());
-})
-$('#days').on('change',()=>{
-    getSess($('#movs').val(),$('#days').val());
-})
-
+//另一個區塊 : booking
+function getBooking(){
+    $('#mov').text($('#movs option:selected').text());
+    $('#day').text($('#days option:selected').val());
+    $('#sess').text($('#session option:selected').val());
+}
     function getMov() {
-        let params = location.href.split("?")[1].split("&");
+        let params = location.href.split("?")[1].split("&");//[do=order , id=xxx]
+
         $.get("./api/get_movie.php", (res) => {
             $('#movs').html(res);
-            if (params[1]) {
+            if (params[1]) {//id=xxx
                 $(`option[value=${params[1].split("=")[1]}]`).attr("selected", true);
             }
-            getDay($('#movs').val());//連動式參數帶入
+            //聯動式呼叫 :
+            getDay($('#movs').val());//初始值
+            $('#movs').on('change',()=>getDay($('#movs').val()))//onchange
         })
+
     }
-    function getDay(id){
-        $.get("api/get_day.php",{id},(res)=>{
+
+    function getDay(id) {
+        $.get("api/get_day.php", {
+            id
+        }, (res) => {
             $('#days').html(res)
+            //聯動式呼叫 :
+            getSess(id, $('#days').val());//初始值
+            $('#days').on('change',()=>getSess(id,$('#days').val()));//onchange
+
         })
     }
-    function getSess(id,date){
-        $.get("api/get_session.php",{id,date},(res)=>{
+
+    function getSess(id, date) {
+        $.get("api/get_session.php", {
+            id,
+            date
+        }, (res) => {
             $('#session').html(res);
         })
     }
--- tt.php
+++ tt.php
@@ -1,4 +1,6 @@
 <?php
 $ondate = "2023-02-11";
-echo strtotime($ondate);
+date_default_timezone_set("Asia/Taipei");
+// echo date("G");
+echo date("Y-m-d G:i:s");
 echo "<br>";
