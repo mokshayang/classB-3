388343af83eed88c6650f87e6c72693d8f3e256e
unknown <dullstart.yang.com>
2023-02-18 22:44:22 +0800

    加油喔

---
+++ front/intro.php
@@ -0,0 +1,26 @@
+﻿<div class="tab rb" style="width:87%;">
+  <div style="background:#FFF; width:100%; color:#333; text-align:left">
+    <video src="movie/03B20v.avi" width="300px" height="250px" controls="" style="float:right;"></video>
+    <font style="font-size:24px"> <img src="Profile page_files/03B20.png" width="200px" height="250px" style="margin:10px; float:left">
+      <p style="margin:3px">影片名稱 ：
+        <input type="button" value="線上訂票" onclick="lof('?do=ord&amp;id=4')" style="margin-left:50px; padding:2px 4px" class="b2_btu">
+      </p>
+      <p style="margin:3px">影片分級 ： <img src="Profile page_files/03C04.png" style="display:inline-block;">限制級 </p>
+      <p style="margin:3px">影片片長 ： 時/分</p>
+      <p style="margin:3px">上映日期 2014/02/14</p>
+      <p style="margin:3px">發行商 ： </p>
+      <p style="margin:3px">導演 ： </p>
+      <br>
+      <br>
+      <p style="margin:10px 3px 3px 3px; word-break:break-all"> 劇情簡介：<br>
+      </p>
+    </font>
+    <table width="100%" border="0">
+      <tbody>
+        <tr>
+          <td align="center"><input type="button" value="院線片清單" onclick="lof('?')"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
---
+++ front/main.php
@@ -0,0 +1,49 @@
+<div class="half" style="vertical-align:top;">
+    <h1>預告片介紹</h1>
+    <div class="rb tab" style="width:95%;">
+        <div id="poster">
+            <div class="lis">
+
+            </div>
+
+
+
+            <div class="con">
+                <div class="lef"></div>
+                <div class="bts">
+                    <?php
+
+
+                    ?>
+                    <div class="bt">
+                        <img src="" alt="">
+                        <div></div>
+                    </div>
+                    <?php //} 
+                    ?>
+                </div>
+                <div class="rig"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="half">
+    <h1>院線片清單</h1>
+    <div class="rb tab" style="width:95%;">
+
+
+
+        <style>
+            .ct a{
+                text-decoration: none;
+            }
+        </style>
+        <div class="ct">
+
+        </div>
+    </div>
+</div>
